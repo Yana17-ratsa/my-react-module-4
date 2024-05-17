2de90adad7af13f48dd57bf34e2b25254671195a
Yana17-ratsa <ratsayana13@gmail.com>
2024-05-17 14:13:06 +0300

    conspect part 1 done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { useState } from 'react';
+import { ArticleList } from '../ArticleList/ArticleList';
+import { fetchArticlesWithTopic } from '../../articles-api';
+import { SearchForm } from '../SearchForm/SearchForm';
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async topic => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
+
+export default App;
